8dc0b24b8ebb52ba995d594e10e77d1f9b9086f4
Josef Fruehwald <JoFrhwld@gmail.com>
2011-09-08 16:24:12 -0400

    Added analysis code, and slides

---
+++ talks/PennState2011/talk/analyse.R
@@ -0,0 +1,185 @@
+eys.stable <- subset(eys, Base == Coda)
+
+eys.stable.coda.mean <- ddply(eys.stable, .(File, DOB, DOB0, Decade, Sex, Age, Year, Coda),
+                              summarise, 
+                              Diag = mean(Diag), .progress = "text")
+                              
+library(MASS)
+ggplot(eys.stable.coda.mean, aes(DOB, Diag, color = Coda)) + 
+            geom_point() +
+            stat_smooth()
+
+eys.stable.coda.mean.c <- cast(eys.stable.coda.mean, 
+                               File + DOB + Decade ~ Coda, 
+                               value = "Diag")
+
+ggplot(eys.stable.coda.mean.c, aes(closed-open, closed-final)) + 
+            geom_point() +
+            geom_abline() +
+            facet_wrap(~Decade) + 
+            coord_fixed()
+
+
+## Statistical tests
+library(lme4)
+
+coda.moder1 <- lmer(Diag ~ I(DOB0/10)*Coda + (Coda | File), data = eys.stable)
+coda.moder2 <- lmer(Diag ~ I(DOB0/10)*Coda + (Coda | File), data = eys.stable.coda.mean)
+coda.mod <- lm(Diag ~ I(DOB0/10)*Coda, data = eys.stable.coda.mean)
+
+
+## looking at manner of articulation
+eys.closed <- subset(eys.stable, Coda %in% c("closed","open"))
+eys.closed <- transform(eys.closed, Manner = as.factor(as.character(Manner)))
+eys.closed <- subset(eys.closed, !is.na(Manner))
+
+eys.closed$Manner <- relevel(eys.closed$Manner, "stop")
+
+
+eys.manner.mean <- ddply(eys.closed, .(File, DOB, Decade, DOB0, Manner), 
+                    summarise,
+                    Diag = mean(Diag))
+
+ggplot(eys.manner.mean, aes(DOB, Diag, color = Manner)) +
+          geom_point() + 
+          stat_smooth()
+
+
+## Statistics
+
+manner.moder1 <- lmer(Diag ~ I(DOB0/10)*Manner + (Manner | File), data = eys.closed)
+maner.moder2 <- lmer(Diag ~ I(DOB0/10)*Manner + (Manner | File), data = eys.manner.mean)
+
+## lateral is clearly very different
+
+
+eys.final <- subset(eys, Base == "final")
+
+cast(eys.final, Coda ~ Morph)
+##     Coda  NULL compound contract deriv inflect
+## 1 closed     1        0      356    31    1222
+## 2  final 19283        0        0     0       0
+## 3 hiatus     0        1        5    30     530
+## 4   open     0      753        0    12       0
+
+eys.final <- subset(eys.final, Morph %in% c("NULL","inflect"))
+eys.final <- subset(eys.final, Word != "A")
+
+eys.final$Context <- NA
+eys.final$Context[with(eys.final, grep("D$", Word))] <- "-ed"
+eys.final$Context[with(eys.final, grep("S.?$", Word))] <- "-s"
+eys.final$Context[with(eys.final, grep("IN.?$", Word))] <- "-ing"
+eys.final$Context[is.na(eys.final$Context)] <- "final"
+
+cast(eys.final, Morph ~ Context, value = "Diag")
+
+eys.final <- subset(eys.final, !(Morph == "NULL" & Context == "-s"))
+
+eys.final$Context <- as.factor(eys.final$Context)
+
+eys.final$Context <- relevel(eys.final$Context, "final")
+
+eys.final.mean <- ddply(eys.final, .(File, DOB, DOB0, Decade, Context),
+                        summarise,
+                        Diag = mean(Diag))
+
+
+ggplot(eys.final.mean, aes(DOB, Diag, color = Context)) + 
+              geom_point() + 
+              stat_smooth()
+
+
+final.moder1 <- lmer(Diag ~ I(DOB0/10)*Context + (Context | File), data = eys.final)
+final.moder2 <- lmer(Diag ~ I(DOB0/10)*Context + (Context | File), data = eys.final.mean)
+final.mod <- lm(Diag ~ I(DOB0/10)*Context, data = eys.final.mean)
+
+
+### final dataset for comparison
+
+eys.compare <- subset(eys, Morph %in% c("inflect","NULL"))
+eys.compare$Context <- eys.compare$Coda
+eys.compare$Context <- as.character(eys.compare$Context)
+eys.compare$Context[eys.compare$Manner == "lateral"] <- "/l/"
+eys.compare$Context <- gsub("open","closed",eys.compare$Context)
+
+eys.compare$Context <- as.factor(eys.compare$Context)
+eys.compare$Context <- relevel(eys.compare$Context, c("final", "/l/", "hiatus","closed"))
+
+eys.compare.mean <- ddply(eys.compare, .(File, DOB, DOB0, Decade, Year, Context),
+                          summarise,
+                          Diag = mean(Diag))
+
+
+
+ggplot(eys.compare.mean, aes(DOB, Diag, color = Context)) + 
+        stat_smooth()
+
+
+context.moder1 <- lmer(Diag ~ I(DOB0/10) * Context + (Context | File), data = eys.compare)
+
+
+
+
+slope.levels <- levels(eys.compare$Context)
+
+## This is a one-off, single use function
+get.mer.slopes <- function(mer, slope.levels){
+    fixefs <- fixef(mer)
+    slopes <- c(0, fixefs[6:8]) + fixefs[2]
+    return(data.frame(Context = slope.levels, coef = slopes))
+}
+
+
+
+## Compare results of a sociolinguistic study to a lab study
+eys.compare.mean <- transform(eys.compare.mean, StudyYear = Decade + 18)
+ggplot(eys.compare.mean, aes(Context, Diag, fill = Context)) + 
+            geom_boxplot() + 
+            facet_wrap(~StudyYear)
+
+
+recent.compare.mean <- subset(eys.compare.mean, DOB >= 1980)
+recent.compare.mean <- transform(recent.compare.mean, Year.2 = floor(DOB/2)*2)
+recent.compare.mean <- transform(recent.compare.mean, StudyYear = Year.2 + 18)
+
+ggplot(recent.compare.mean, aes(Context, Diag, color = Context)) + 
+      geom_line(aes(group = File), color = "grey") +
+      geom_point()  + 
+      facet_wrap(~StudyYear)+
+      theme_bw()
+
+mean(ddply(eys.compare, .(Year), summarise, range = diff(range(Age)))$range)
+#54
+
+out <- vector(mod = "list", length = length(0:(max(eys.compare$DOB0)-50)))
+
+
+
+for(i in 0:(max(eys.compare$DOB0)-50)){
+  df <- subset(eys.compare, DOB0 >= i & DOB0 < i+50 )
+  mod <- lmer(Diag ~ I(DOB0/10)*Context + (Context | File), data = df)
+  slopes <- get.mer.slopes(mod, levels(df$Context))
+  slopes$Iter <- i
+  out[[i+1]] <- slopes
+  cat(i)
+  cat(" ")
+}
+
+n.out <- vector(mod = "list", length = length(0:(max(eys.compare$DOB0)-50)))
+for(i in 0:(max(eys.compare$DOB0)-50)){
+  df <- subset(eys.compare, DOB0 >= i & DOB0 < i+50 )
+  n.out[[i+1]] <- data.frame(Iter = i, N = length(unique(df$File)) )
+}
+
+ldply(out, function(x)x)->out.df
+ldply(n.out, function(x)x)->n.out.df
+merge(out.df, n.out.df)->out.df
+
+out.df$Context <- relevel(out.df$Context,  c("final", "/l/", "hiatus","closed"))
+
+ggplot(out.df, aes((Iter+1888) + 68 , coef, color = Context)) +
+        geom_hline(y = 0)+
+        geom_line()+ 
+        xlab("Study Year") 
+
+
--- talks/PennState2011/talk/prep.R
+++ talks/PennState2011/talk/prep.R
@@ -9,11 +9,99 @@ setwd("~/Documents/Classes/jofrhwld.github.com/talks/PennState2011/talk/")
 
 eys <- subset(all_philly, VClass %in% c("ey","eyF"))
 
-## I have coded some ey words previously, but we've added more speakers since then
+## I have coded some ey words previously, but we've added more speakers since then.
 eys.cod <- read.delim("data/eys.coding.txt")
 ey.words <- as.character(unique(eys$Word))
 non.coded <- ey.words[!ey.words %in% eys.cod$Word]
-write.table(cbind(non.coded), "data/tocode.txt", sep = "\t", row.names = F, quote = F)
+#write.table(cbind(non.coded), "data/tocode.txt", sep = "\t", row.names = F, quote = F)
+eys.cod.add <- read.delim("data/tocode.txt")
+
+eys.cod <- rbind(eys.cod[,c("Word","Coda","Base","Morph")], eys.cod.add)
+
+## Sanity checks before merge
+nrow(eys)
+# 42016
+
+sum(!eys$Word %in% eys.cod$Word)
+# 0
+
+nrow(merge(eys, eys.cod))
+# 42026
+
+eys <- merge(eys, eys.cod)
+
+## Post merge sanity check
+nrow(eys)
+# 42026
+
+## Eliminate words preceded by *, indicating annotators were unsure of annotation
+length(grep("\\*", eys$Word))
+# 16
+
+eys <- eys[-grep("\\*", eys$Word),]
+
+## Eliminate words which are NA or ? for Coda coding
+with(eys, sum(is.na(Coda)|Coda == "?"))
+# 115
+
+eys <- subset(eys, !(is.na(Coda) | Coda == "?"))
+eys <- subset(eys, !(is.na(Base) | Base == "?"))
+
+## cleaned data
+nrow(eys)
+# 41911
 
 
+## Cleaning coding typos
+levels(eys$Base)
+# [1] "?"      "closed" "free"   "hiatus" "open"   "final"  "open " 
 
+levels(eys$Base) <- gsub(" ", "", levels(eys$Base))
+levels(eys$Base) <- gsub("free", "final", levels(eys$Base))
+
+levels(eys$Coda)
+# [1] "?"      "closed" "free"   "hiatus" "open"   "final"  "open "
+
+levels(eys$Coda) <- gsub(" ", "", levels(eys$Coda))
+
+
+### Eliminate extreaneous levels
+
+as.factor(as.character(eys$Base)) -> eys$Base
+as.factor(as.character(eys$Coda)) -> eys$Coda
+
+
+### Create Diag measure
+eys <- transform(eys, Diag = F2.n - F1.n, Diag2 = F2.n - (2*F1.n))
+
+### Evaluate Diag
+eys$VClass <- as.character(eys$VClass)
+eys.mean <- ddply(eys, .(File, DOB, VClass), summarise, 
+                  F1 = mean(F1.n), 
+                  F2 = mean(F2.n), 
+                  Diag = mean(Diag),
+                  Diag2 = mean(Diag2))
+
+compare_diag <- expand.grid(F1 = seq(-1.06, 0.75, length = 100), Diag = seq(-0.21, 2.23, length = 10), Diag2 = seq(-0.96, 3.31, length = 10) )
+
+compare_diag <- transform(compare_diag, F2.1 = Diag + F1, F2.2 = Diag2 + (F1*2))
+
+
+ggplot(compare_diag, aes(-F2.1, -F1, group = Diag)) + 
+        geom_point(data = eys.mean, aes(x = -F2,  color = VClass)) +
+        geom_line() + 
+        xlim(-1.48, -0.21) + 
+        coord_fixed()
+
+ggplot(compare_diag, aes(-F2.2, -F1, group = Diag2)) + 
+        geom_point(data = eys.mean, aes(x = -F2,  color = VClass)) +
+        geom_line() + 
+        xlim(-1.48, -0.21) + 
+        coord_fixed()
+
+
+## Diag is a better fit
+
+
+## Create DOB variants
+eys <- transform(eys, Decade = floor(DOB/10)*10, DOB0 = DOB-min(DOB))
