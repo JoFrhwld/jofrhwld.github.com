d43aed7e3c6c9fd14a6117f3eb20d25829dbfc07
Josef Fruehwald <JoFrhwld@gmail.com>
2012-03-27 21:03:28 -0400

    loading and massaging data

---
+++ load_data.R
@@ -0,0 +1,37 @@
+## Load the data
+philly <- read.csv("Murders in Philadelphia County 1988 - 2011.csv")
+
+## Process the Hour of the day
+philly$half <- NA
+philly$half[grepl("[aA]", philly$time, )] <- "AM"
+philly$half[grepl("[pP]", philly$time, )] <- "PM"
+philly$hour <- gsub("[aApP][mM]?", "", philly$time)
+philly$hour <- as.numeric(philly$hour)
+philly$hour[philly$hour > 12] <- NA
+philly$hour[is.na(philly$half)] <- NA
+philly$hour[philly$hour == 12 & !is.na(philly$hour)] <- 0
+philly$hour[philly$half == "PM" 
+						& !is.na(philly$half)] <- philly$hour[philly$half == "PM" & 
+																									!is.na(philly$half)] + 12
+
+## Hour of day centered with midnight as 0
+philly.hour$hour2 <- philly.hour$hour
+philly.hour$hour2[philly.hour$half == "PM"] <- philly.hour$hour[philly.hour$half == "PM"] - 24
+
+## Fill in missing hours as midnight
+philly$hour3 <- philly$hour
+philly$hour3[is.na(philly$hour)] <- 0
+philly$date <- paste(philly$date, philly$hour3)
+
+
+## Get various date information
+dates <- as.POSIXlt(philly$date, format = "%m/%d/%y %H")
+philly$date <- as.Date(dates)
+philly$month <- month.abb[dates$mon+1]
+philly$year <- dates$year + 1900
+philly$monthn <- dates$mon + 1
+
+## Month date indicates just the month in Date format
+philly$month.date <- as.POSIXct(paste(dates$mon + 1,1, dates$year + 1900, sep = "/"), format = "%m/%d/%Y")
+
+
